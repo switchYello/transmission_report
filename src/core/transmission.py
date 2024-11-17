02b12c9b1377b48e10500dd99a72a87445c0eead
huangcy_sh <huangcy_sh@ushareit.com>
2024-11-17 10:19:45 +0800

    完善tr的api接口，优化日志输出

--- src/core/transmission.py
+++ src/core/transmission.py
@@ -20,15 +20,15 @@ class Transmission:
         # base验证
         if len(self._username) > 0 and len(self._password) > 0:
             session.auth = HTTPBasicAuth(self._username, self._password)
-        resp = session.post(self._host + '/transmission/rpc', data=data_, timeout=10)
+        resp = session.post(self._host + '/transmission/rpc', data=data_, timeout=20)
         if resp.status_code == 409:  # 如果是409则添加请求头后重新请求重新请求
             sessionId = resp.headers['x-transmission-session-id']
             session.headers.update({'X-Transmission-Session-Id': sessionId})
             resp = session.post(self._host + '/transmission/rpc', data=data_)
         if resp.status_code == 401:
             raise Exception('username or password is incorrect')
-        # 解析响应报文
-        if not resp.status_code == 200:
-            raise Exception("请求tr失败:", resp.text)
-        torrent_list: list = resp.json()['arguments']['torrents']
+        # 解析响应报文,若果响应头不是200 或者包含特定的字符，说明失败了
+        if not resp.status_code == 200 or "<!DOCTYPE HTML>" in resp.text:
+            raise Exception("transmission返回结果不正确:", resp.text)
+        torrent_list: list = resp.json().get('arguments', {}).get('torrents', [])
         return torrent_list
